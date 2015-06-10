e6fee5d53d0eee59f17bcf4e99bbf681913ce756
Jordan Chase <jchase@bostoninteractive.com>
2015-06-10 11:31:32 -0400

    syncing with foundation starter

--- gruntfile.js
+++ gruntfile.js
@@ -1,117 +1,129 @@
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
 
-  // All configuration goes here 
+  var theme_name = 'theme';
+  var base_theme_path = '';
+
+  var global_vars = {
+    theme_name: theme_name,
+    theme_css: 'css',
+    theme_scss: 'sass',
+    base_theme_path: base_theme_path
+  };
+
+  // array of javascript libraries to include.
+  var jsLibs = [
+    '<%= global_vars.base_theme_path %>/js/vendor/placeholder.js',
+    '<%= global_vars.base_theme_path %>/js/vendor/fastclick.js'
+  ];
+
+  // array of foundation javascript components to include.
+  var jsFoundation = [
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.abide.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.accordion.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.alert.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.clearing.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.dropdown.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.equalizer.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.interchange.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.joyride.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.magellan.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.offcanvas.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.orbit.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.reveal.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.slider.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tab.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.tooltip.js',
+    '<%= global_vars.base_theme_path %>/js/foundation/foundation.topbar.js'
+  ];
+
+  // array of custom javascript files to include.
+  var jsApp = [
+    'js/*.js'
+  ];
+
   grunt.initConfig({
+    global_vars: global_vars,
     pkg: grunt.file.readJSON('package.json'),
-
-    uglify: {
-      dist: {
-        files: {
-          'js/plugins.js': ['js/plugins/*.js'],
-          'js/shims.js': ['js/shims/*.js'],
-          'js/theme.min.js': ['js/theme.js']
-        },
+    sass: {
+			dist: {
+  			files :  {
+    			'css/theme.css': 'sass/theme.scss'
+    		},
+				options: {
+					sassDir: 'sass',
+					cssDir: 'css',
+					sourcemap : true,
+					includePaths: require('node-bourbon').includePaths
+				},
+			},
+		},
+    drush: {
+      cc_theme_registry: {
+        args: ['cc', 'theme-registry']
+      },
+      cc_registry: {
+        args: ['cc', 'registry']
+      },
+      cc_css_js: {
+        args: ['cc', 'css-js']
+      },
+      cc_all: {
+        args: ['cc', 'all']
       }
-    }, // End Uglify
-
-    imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'img/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'img/'
-        }]
-      }
-    }, // End Imagemin
-
-    compass: {
-      dist: {
-        options: {
-          sassDir: 'sass',
-          cssDir: 'css',
-          output: 'compressed'
-        }
-      }
-    }, // End Compass
-
-    jshint: {
-      files: ['gruntfile.js', 'js/theme.js'],
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    }, // End JsHint
-
-    db_dump: {
-      "local": {
-        "options": {
-          "title": "Local DBs",
-        
-          // default config for VLAD stack
-          "database": "dbName",
-          "user": "vlad",
-          "pass": "vlad",
-          "host": "192.168.100.100",
-          
-          
-          "backup_to": "db/backups/local.sql"
+    },
+    webfont: {
+      icons : {
+        src: 'images/icons/*.svg',
+        dest: 'fonts/custom-icons',
+        destCss : 'sass',
+        options : {
+          stylesheet : 'scss',
+          relativeFontPath : '../fonts/custom-icons',
+          htmlDemo : 'false'
         }
       }
     },
-
-    uncss: {
-      dist: {
-        files: {
-          'css/theme.clean.css': ['*.html']
+    notify : {
+      sass : {
+        options : {
+          title : "CSS Compiled",
+          message : "SASS Task Complete"
+        }
+      },
+      drush : { 
+        options : {
+          title : "Cache Cleared",
+          message : "Cleared the Drupal cache"
         }
       }
     },
-
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint'],
-      options: {
-        livereload: true,
-      }, 
-      compass: {
-        files: ['sass/{,*/}*.{scss,sass}'],
-        tasks: ['compass']
-      },
-      html: {
-        files: ['**/*.html','**/*.css'],
-        options: {
-            livereload: true
-        },
-      },
-      php: {
-        files: ['**/*.php'],
+      grunt: { files: ['Gruntfile.js'] },
+      sass: {
+        files: ['<%= global_vars.theme_scss %>/*.scss','<%= global_vars.theme_scss %>/*/*.scss'],
+        tasks: ['sass', 'notify:sass', 'drush:cc_css_js', 'notify:drush'],
         options: {
           livereload: true
-        },
+        }
+      },
+      templates: {
+        files: ['**/*.tpl.php', '**/*.info', 'template.php'],
+        tasks: ['drush:cc_theme_registry', 'notify:drush']
+      },
+      info :{
+        files: ['**/*.info', 'template.php'],
+        tasks: ['drush:cc_registry', 'notify:drush']
+      },
+      svg : {
+        files : ['images/icons/source/*.svg'],
+        tasks : ['webfont','sass']
       }
-    }, // End Watch
-
-  }); // End Configuration
-
-  // Plugin references
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mysql-dump');
-  grunt.loadNpmTasks('grunt-uncss');
-
-
-  // Task registration
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['uglify', 'imagemin', 'jshint']);
-  grunt.registerTask('backup',['db_dump']);
-  grunt.registerTask('tidy',['uncss', 'uglify', 'imagemin']);
-
+    }
+  });
+  grunt.registerTask('build', ['sass', 'notify', 'grunticon']);
+  grunt.registerTask('default', ['build', 'watch']);
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-webfont');
 };
--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,41 @@
 (function ($) {
+  
+  // set the default foundation breakpoints and variables
+  var breakpoints = {
+    sm : 640,
+    md : 1025,
+    lg : 1441,
+    xl : 1921
+  },
+  winWidth,
+  winHeight,
+  docWidth,
+  docHeight;
+  
+  
+  /**
+   * Page initialization
+   * Called on resize, page load, or to reset the page
+   */ 
+  function init(){
+    setVars();
+    setPositions();
+  }
+  
+  function setVars(){ 
+    docHeight = $(document).outerHeight(),
+    docWidth  = $(document).outerWidth(),
+    winHeight = $(window).height(),
+    winWidth  = $(window).width();
+  }
+  
+  /**
+   * Sets any positioning on the page
+   */
+  function setPositions(){
+    // Place positioning code here
+  }
+
   /**
    * Swap out svg files for PNGs on unsupporting devices. Modrnizr determines
    * what an unsupporting device is by adding the .no-svg class to the html
@@ -12,31 +49,11 @@
     });
   }
 
-  /**
-   * Stuff to run immediately upon page load
-   */
-  svgToPng();
-
-  // initialize foundation plugins
-  $(document).foundation();
-
-  /**
-   * Stuff to run on resize.
-   */
-  $(window).resize(function() {
-    waitForFinalEvent(function() {
-      var width = $(window).width();
-      // Place functions here
-    }, 500, "");
-  });
-
-  /**
-   * Stuff to run after page load is complete.
-   */
-  $(document).ready(function() {
-    // Place functions here
-  });
-
+  
+  function toggleNav(){
+    $(".nav-toggle, .off-canvas, body").toggleClass("open");
+  }
+  
   /**
    * Helper function to delay firing resize events until the user actually
    * stops resizing their browser.
@@ -53,5 +70,39 @@
       timers[uniqueId] = setTimeout(callback, ms);
     };
   })();
+  
+  Drupal.behaviors.STARTER = {
+    attach: function(context, settings) {
+      
+      // Initialize plugins/page  
+      $(document).foundation();
+      svgToPng();
+      
+      // Initialize page
+      init();
+      
+      /**
+       * Event listeners
+       */
+   
+      $(".js__nav-toggle").on("click", function(e){
+         e.stopPropagation();
+         toggleNav();
+       });
+
+      $(window).resize(function() {
+        waitForFinalEvent(function() {
+          init();
+          // close the nav if it's open
+          if($("body").hasClass("open")){
+            toggleNav();
+          }
+        }, 200, "global");
+      });
+    }
+  };
+  
+  
 
 }(jQuery));
+
